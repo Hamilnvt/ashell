897e1767370533556f378548d6508f8932303448
Mathieu Linty <mathieu.linty@studio.unibo.it>
2025-02-27 15:52:17 +0100

    Feat: better shlog and docStrings

--- main.c
+++ main.c
@@ -6,20 +6,17 @@
     - grep
 
     Urgent stack:
-    - per non avere tutto quel casino in exec_doc: fare un array di struct {doc, usage, info} e indirizzarlo con i CmdType + funzione che preso il CmdType stampa tutto
-    - shlog senza newline (quello con la newline rimane shlog(...))
+    /
 
     Altre cose da fare:
     - permissions (mostrarle in ls, anche con colori diversi dei file; se si vuole eseguire un'operazione per cui non si hanno i permessi bisogna segnalarlo all'utente)
     - ed text editor https://www.gnu.org/software/ed/manual/ed_manual.html
-    - print documentation
-    - unit testing script
     - previous and next command
     - redirection (>)
       > probabilmente devo tenere un buffer che poi verra' ridirezionato (solitamente verso stdin)
     - config file che permette di modificare i colori e chissa' cos'altro
     - correction if a similar command is found
-    - autocompletion (chissa' se e' possible)
+    - autocompletion by tabbing (chissa' se e' possible)
 */
 
 #include <stdio.h>
@@ -84,7 +81,9 @@ typedef enum
 } Shlog_Levels;
 
 static char shlog_buffer[1024];
-void shlog(Shlog_Levels lvl, char *format, ...)
+
+void shlog(Shlog_Levels lvl, char *format, ...); // Forward declaration
+void shlog_NO_NEWLINE(Shlog_Levels lvl, char *format, ...)
 {
     va_list msg_fmt; 
     va_start(msg_fmt, format);
@@ -105,14 +104,27 @@ void shlog(Shlog_Levels lvl, char *format, ...)
     }
 
     if (strchr(shlog_buffer, '\n')) {
+        shlog(lvl, shlog_buffer);
+    } else printf("%s", shlog_buffer);
+}
+void shlog(Shlog_Levels lvl, char *format, ...)
+{
+    va_list msg_fmt; 
+    va_start(msg_fmt, format);
+    vsprintf(shlog_buffer, format, msg_fmt);
+    va_end(msg_fmt);
+    if (strchr(shlog_buffer, '\n')) {
         char *line = strtok(shlog_buffer, "\n");
-        printf("%s\n", line);
+        shlog(lvl, line);
         line = strtok(NULL, "\n");
         while (line) {
             shlog(lvl, line);
             line = strtok(NULL, "\n");
         }
-    } else printf("%s\n", shlog_buffer);
+    } else {
+        shlog_NO_NEWLINE(lvl, shlog_buffer);
+        printf("\n");
+    }
 }
 
 void shlog_just_use_doc_for_args(char *cmd_name) { shlog(SHLOG_INFO, "`doc %s -u` to know more about `%s` usage", cmd_name, cmd_name); }
@@ -122,14 +134,19 @@ void shlog_unknown_flag(char *flag, char *cmd_name)
     shlog(SHLOG_ERROR, "Unknown flag `%s` for command `%s`", flag, cmd_name);
     shlog_just_use_doc_for_flags(cmd_name);
 }
-void shlog_unknown_flag_for_arg(char *flag, char *cmd_name, char *arg)
+void shlog_unknown_flag_for_single_arg(char *flag, char *cmd_name)
 {
-    shlog(SHLOG_ERROR, "Unknown flag `%s` for command `%s %s`", flag, cmd_name, arg);
+    shlog(SHLOG_ERROR, "Unknown flag `%s` for command `%s <arg>`", flag, cmd_name);
+    shlog_just_use_doc_for_flags(cmd_name);
+}
+void shlog_unknown_flag_for_multiple_args(char *flag, char *cmd_name)
+{
+    shlog(SHLOG_ERROR, "Unknown flag `%s` for command `%s <...args>`", flag, cmd_name);
     shlog_just_use_doc_for_flags(cmd_name);
 }
 void shlog_unknown_command(char *cmd_name) {
     shlog(SHLOG_ERROR, "Unknown command `%s`", cmd_name);
-    shlog(SHLOG_INFO, "`doc` for a comprehensive list of commands");
+    shlog(SHLOG_INFO, "`doc -c` for a comprehensive list of commands");
 }
 void shlog_no_flags_for_this_command(char *cmd_name)
 {
@@ -224,21 +241,7 @@ typedef enum
     CMD_SL,
     CMDTYPES_COUNT 
 } CmdType;
-
-static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive documentation");
-#define DOC ("DOCUMENTATION\n \n"\
-             "Commands:\n "\
-             "    Commands have a name, a series of arguments and a series of flags: name [...args] [...flags]\n"\
-             "    - Command with no arguments:   name\n"\
-             "    - Command with one argument:   name arg\n"\
-             "    - Command with more arguments: name arg1 arg2\n"\
-             " \n"\
-             "Flags:\n "\
-             "    Flags ought to go after the arguments of the command.\n"\
-             "    - Flags with no arguments:   -flag\n"\
-             "    - Flags with one argument:   -flag=arg\n"\
-             "    - Flags with more arguments: -flag=arg1,arg2\n"\
-     )
+static_assert(CMD_UNKNOWN==0 && CMD_DOC==1 && CMD_ECHO==2 && CMD_LS==3 && CMD_CD==4 && CMD_PWD==5 && CMD_MKDIR==6 && CMD_RM==7 && CMD_QUIT==8 && CMD_CLEAR==9 && CMD_SL==10 && CMDTYPES_COUNT==11, "Order of commands is preserved"); 
 
 int count_words(char *input)
 {
@@ -313,22 +316,22 @@ void cmd_print(Command cmd)
     shlog(SHLOG_DEBUG, "  argc = %d,", cmd.argc);
     if (cmd.argc == 0) shlog(SHLOG_DEBUG, "  argv = [],");
     else  {
-        shlog(SHLOG_DEBUG, "  argv = [,");
+        shlog_NO_NEWLINE(SHLOG_DEBUG, "  argv = [ ");
         for (int i = 0; i < cmd.argc; i++) {
-            shlog(SHLOG_DEBUG, "%s", cmd.argv.items[i]);
+            printf(cmd.argv.items[i]);
             if (i != cmd.argc - 1) printf(", ");
         }
-        shlog(SHLOG_DEBUG, "  ],");
+        printf(" ]\n");
     }
     shlog(SHLOG_DEBUG, "  flagc = %d,", cmd.flagc);
     if (cmd.flagc == 0) shlog(SHLOG_DEBUG, "  flagv = []");
     else {
-        shlog(SHLOG_DEBUG, "  flagv = [");
+        shlog_NO_NEWLINE(SHLOG_DEBUG, "  flagv = [ ");
         for (int i = 0; i < cmd.flagc; i++) {
-            shlog(SHLOG_DEBUG, "%s", cmd.flagv.items[i]);
+            printf(cmd.flagv.items[i]);
             if (i != cmd.flagc - 1) printf(", ");
         }
-        shlog(SHLOG_DEBUG, "  ]");
+        printf(" ]\n");
     }
     shlog(SHLOG_DEBUG, "}");
 }
@@ -354,7 +357,7 @@ CmdType getCmdType(char *cmd_name)
 bool words_to_command(ArrayOfStrings words, Command *cmd)
 {
     if (words.count == 0) {
-        shlog(SHLOG_ERROR, "no command provided");
+        shlog(SHLOG_FATAL, "no command provided");
         return false;
     }
     bool parsing_args = false;
@@ -365,8 +368,10 @@ bool words_to_command(ArrayOfStrings words, Command *cmd)
         if (i == 0) {
             cmd->name = strdup(words.items[0]);
             cmd->type = getCmdType(words.items[0]);
-            if (cmd->type == CMD_UNKNOWN)
+            if (cmd->type == CMD_UNKNOWN) {
+                shlog_unknown_command(cmd->name);
                 return false;
+            }
             parsing_args = true;
         } else {
             if (parsing_args) {
@@ -416,8 +421,7 @@ bool is_dir(char *dir_path)
     return true;
 }
 
-// Usage flags and doc ///////////////////////////
-// TODO introduce flags usage (with a nice format, magari mettere le flag di ogni comand in una macro e fare uno SHLOG_FLAGS)
+// Usage, flags and doc ///////////////////////////
 static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive usage for all commands");
 #define USAGE_DOC   "doc [command] [...flags]"
 #define USAGE_ECHO  "echo <...args>"
@@ -431,12 +435,12 @@ static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive usage for all commands");
 
 #define NO_FLAGS "No flags for this command."
 static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive flags for all commands");
-#define FLAGS_DOC   ("-d\tPrint documentation for the shell\n"\
-                     "-c\tPrint list of available commands\n"\
-                     "<arg> -d\tPrint doc info for <arg>\n"\
-                     "<arg> -u\tPrint usage info for <arg>\n"\
-                     "<arg> -f\tPrint flags info for <arg>\n")
-#define FLAGS_LS    "-h\tShow hidden files"
+#define FLAGS_DOC   ("-d\t\tPrint (d)ocumentation for the shell\n"\
+                     "-c\t\tPrint list of available (c)ommands\n"\
+                     "<command> -d\tPrint (d)oc info for <command>\n"\
+                     "<command> -u\tPrint (u)sage info for <command>\n"\
+                     "<command> -f\tPrint (f)lags info for <command>\n")
+#define FLAGS_LS    "-h\tShow (h)idden files"
 #define FLAGS_CD    NO_FLAGS
 #define FLAGS_PWD   NO_FLAGS
 #define FLAGS_MKDIR NO_FLAGS
@@ -456,6 +460,22 @@ static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive doc string for all commands");
 #define DOC_CLEAR "Clear the screen of the terminal."
 #define DOC_QUIT  "Exit the shell safely."
 
+static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive documentation");
+#define DOC ("DOCUMENTATION\n \n"\
+             "Commands:\n "\
+             "    Commands have a name, a series of arguments and a series of flags: name [...args] [...flags]\n"\
+             "    - Command with no arguments:   name\n"\
+             "    - Command with one argument:   name arg\n"\
+             "    - Command with more arguments: name arg1 arg2\n"\
+             " \n"\
+             "Flags:\n "\
+             "    Flags ought to go after the arguments of the command.\n"\
+             "    - Flags with no arguments:   -flag\n"\
+             "    - Flags with one argument:   -flag=arg\n"\
+             "    - Flags with more arguments: -flag=arg1,arg2"\
+     )
+
+static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive commands list");
 #define CMDS_LIST  ("AVAILABLE COMMANDS\n \n"\
                     "  Utility:\n"\
                     "------------------------------------------------------------------------------------------------------\n"\
@@ -475,8 +495,30 @@ static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive doc string for all commands");
                     "------------------------------------------------------------------------------------------------------\n"\
                     "  - quit    \t" DOC_QUIT  "\n"\
                     "  - `ctrl+C`\tExit the shell with a SIGINT.\n"\
-                    "------------------------------------------------------------------------------------------------------\n"\
+                    "------------------------------------------------------------------------------------------------------"\
         )
+typedef struct
+{
+    char *doc;
+    char *usage;
+    char *flags;
+} DocStrings;
+
+static_assert(CMDTYPES_COUNT == 10+1, "Exhaustive doc_strings");
+DocStrings doc_strings[CMDTYPES_COUNT+1] = {
+    {},                                                         // unknown,
+    { .doc=DOC_DOC,   .usage=USAGE_DOC,   .flags=FLAGS_DOC   }, // doc,
+    { .doc=DOC_ECHO,  .usage=USAGE_ECHO,  .flags=FLAGS_ECHO  }, // echo,
+    { .doc=DOC_LS,    .usage=USAGE_LS,    .flags=FLAGS_LS    }, // ls,
+    { .doc=DOC_CD,    .usage=USAGE_CD,    .flags=FLAGS_CD    }, // cd,
+    { .doc=DOC_PWD,   .usage=USAGE_PWD,   .flags=FLAGS_PWD   }, // pwd,
+    { .doc=DOC_MKDIR, .usage=USAGE_MKDIR, .flags=FLAGS_MKDIR }, // mkdir,
+    { .doc=DOC_RM,    .usage=USAGE_RM,    .flags=FLAGS_RM    }, // rm,
+    { .doc=DOC_QUIT,  .usage=USAGE_QUIT,  .flags=FLAGS_QUIT  }, // quit,
+    { .doc=DOC_CLEAR, .usage=USAGE_CLEAR, .flags=FLAGS_CLEAR }, // clear,
+    {},                                                         // sl,
+    {}                                                          // cmdtypes_count 
+};
 //////////////////////////////////////////////////
 
 #define DOC_FLAG_DOC   (1 << 3)
@@ -504,7 +546,7 @@ void exec_doc(Command *cmd)
             if      (streq(flag, "-u")) doc_flags |= DOC_FLAG_USAGE;
             else if (streq(flag, "-f")) doc_flags |= DOC_FLAG_FLAGS;
             else {
-                shlog_unknown_flag_for_arg(flag, cmd->name, cmd->argv.items[0]);
+                shlog_unknown_flag_for_single_arg(flag, cmd->name);
                 return;
             }
         }
@@ -524,59 +566,17 @@ void exec_doc(Command *cmd)
     } else if (cmd->argc == 1) {
         char *cmd_to_doc_name = cmd->argv.items[0];
         CmdType type = getCmdType(cmd_to_doc_name);
-        switch (type) {
-            case CMD_DOC:
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_DOC);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_DOC);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_DOC);   
-                break;
-            case CMD_ECHO:    
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_ECHO);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_ECHO);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_ECHO);   
-                break;
-            case CMD_LS:      
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_LS);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_LS);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_LS);   
-                break;
-            case CMD_CD:      
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_CD);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_CD);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_CD);   
-                break;
-            case CMD_PWD:     
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_PWD);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_PWD);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_PWD);   
-                break;
-            case CMD_MKDIR:   
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_MKDIR);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_MKDIR);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_MKDIR);   
-                break;
-            case CMD_RM:      
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_RM);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_RM);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_RM);   
-                break;
-            case CMD_CLEAR:
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_CLEAR);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_CLEAR);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_CLEAR);   
-                break;
-            case CMD_QUIT:
-                if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC, DOC_QUIT);
-                if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, USAGE_QUIT);   
-                if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, FLAGS_QUIT);   
-                break;
-            case CMD_UNKNOWN: shlog_unknown_command(cmd_to_doc_name); break;
-            case CMD_SL: break;
-            case CMDTYPES_COUNT: default: shlog(SHLOG_FATAL, "Unreachable"); abort();
+        if      (type == CMD_UNKNOWN) shlog_unknown_command(cmd_to_doc_name);
+        else if (type == CMD_SL) { } // magari metto un easter anche qua
+        else if (type == CMDTYPES_COUNT || type > CMDTYPES_COUNT) {
+            shlog(SHLOG_FATAL, "Unreachable");
+            abort();
+        } else {
+            if (doc_flags & DOC_FLAG_DOC  ) shlog(SHLOG_DOC,   doc_strings[type].doc);
+            if (doc_flags & DOC_FLAG_USAGE) shlog(SHLOG_USAGE, doc_strings[type].usage);   
+            if (doc_flags & DOC_FLAG_FLAGS) shlog(SHLOG_FLAGS, doc_strings[type].flags);   
         }
-    } else {
-        shlog_too_many_arguments(cmd->name);
-    }
+    } else shlog_too_many_arguments(cmd->name);
 }
 
 void exec_echo(Command *cmd)
@@ -764,8 +764,8 @@ int main(void)
                 case CMD_QUIT:  exec_quit();          break;
                 case CMD_CLEAR: exec_clear();         break;
                 case CMD_SL:    exec_sl();            break;
-                case CMD_UNKNOWN: shlog_unknown_command(command.name); break;
                 case CMDTYPES_COUNT: shlog(SHLOG_WARNING, "`%s` command is not implemented yet.", command.name); break;
+                case CMD_UNKNOWN:
                 default: shlog(SHLOG_FATAL, "Unreachable"); abort();
             }
             cmd_free(&command);
